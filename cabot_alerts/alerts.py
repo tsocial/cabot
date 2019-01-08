f1d5331ff90e0b6dfb77d60b5cf715c5299e65a2
akshay-sri123 <akshay.srivastava.95@gmail.com>
2019-01-08 09:36:53 +0530

    Creates alerts, then services and then attaches alerts to services.

--- cabot_alerts/alerts.py
+++ cabot_alerts/alerts.py
@@ -1,6 +1,6 @@
 import argparse
-import csv
 import json
+from collections import defaultdict
 
 import requests
 
@@ -34,7 +34,7 @@ def create_check(url, host, check_name, check_query, check_type, check_value):
               "active": %s,
               "importance": "%s",
               "frequency": %d,
-              "debounce": %d,
+              "debounce": %d, 
               "calculated_status": "%s",
               "query": "%s",
               "host": "%s",
@@ -64,6 +64,13 @@ def check_alert_exists(check_name, url):
     return response.text
 
 
+def check_service_exists(url, service_name):
+    get_url = url + "/api/services/?name=%s" % service_name
+    headers = {'Content-Type': "application/json"}
+    response = requests.get(get_url, headers=headers, auth=(user, password))
+    return response.text
+
+
 def read_checks_file(file_path):
     with open(file_path) as file:
         checks = file.readlines()
@@ -75,18 +82,57 @@ def generate_checks(url, host, config_file):
     check_list = read_checks_file(config_file)
     for check in check_list:
         check_params = check.split(sep="|")
-        create_check(url, host, check_params[0],
-                     fix_cabot_query(check_params[1]), check_params[2],
-                     check_params[3])
+        create_check(url, host, check_params[1], fix_cabot_query(check_params[2]),
+                     check_params[3], check_params[4])
+
+
+def get_alert_id_with_service(url, alert_name, service_name, service_list):
+    check_id = json.loads(check_alert_exists(alert_name, url))[0]["id"]
+    service_list[service_name].append(check_id)
+
+
+def get_service_with_id(url, config_file):
+    service_dict = defaultdict(list)
+    check_list = read_checks_file(config_file)
+    for check in check_list:
+        check_params = check.split(sep="|")
+        get_alert_id_with_service(url, check_params[1], check_params[0], service_dict)
+    return dict(service_dict)
+
+
+def create_service(service_url, service_name, alerts):
+    headers = {'Content-Type': "application/json"}
+    service_data = """{
+                "name": "%s",
+                "users_to_notify": ["1"],
+                "alerts_enabled": true,
+                "status_checks": %s,
+                "alerts": ["1"],
+                "hackpad_id": "",
+                "url": "",
+                "instances": [],
+                "overall_status": "PASSING"
+            }""" % (service_name, alerts)
+
+    print("Creating service for %s" % service_name)
+    response = requests.post(service_url, data=service_data, headers=headers, auth=(user, password))
+    return response.status_code
+
+
+def create_service_with_alerts(url, config_file):
+    service_url = url + "/api/services/?format=api"
+    services_with_id = get_service_with_id(url, config_file)
+    for service in services_with_id.items():
+        if check_service_exists(url, service[0]) == '[]':
+            create_service(service_url, service[0], service[1])
 
 
 if __name__ == '__main__':
     parser = vars(parse_args().parse_args())
-
     url = str(parser["url"])
     host = str(parser["host"])
     config_file = str(parser["config_file"])
     user = str(parser["user"])
     password = str(parser["password"])
-
     generate_checks(url, host, config_file)
+    create_service_with_alerts(url, config_file)
